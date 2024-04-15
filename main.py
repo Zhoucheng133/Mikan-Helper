e34023879698416920559fc1653f509529526e53
zhoucheng <zhoucheng133@outlook.com>
2024-04-15 18:19:31 +0800

    完成功能

--- main.py
+++ main.py
@@ -1,4 +1,67 @@
 import parameters
+import requests
+import feedparser
+import asyncio
+
+
+class Server:
+    pre_ls=[]
+    ls=[]
+
+    def downloadHandler(self, url):
+        data=f'''
+{{
+    "jsonrpc": "2.0",
+    "method": "aria2.addUri",
+    "id": 1,
+    "params": [
+        "token:{parameters.aria_secret}",
+        ["{url}"],
+        {{
+            "split": "5",
+            "max-connection-per-server": "5",
+            "seed-ratio": "0"
+        }}
+    ]
+}}
+        '''
+        requests.post(parameters.aria_link, data)
+
+    def judge(self):
+        newItems = [item for item in self.ls if item not in self.pre_ls]
+        download_ls=[]
+        for item in newItems:
+            if not any(exclude in item['title'] for exclude in parameters.exclude):
+                if any(item['title'].startswith(prefix) for prefix in parameters.start_with):
+                    download_ls.append(item)
+        for item in download_ls:
+            print("下载: "+item["title"])
+            self.downloadHandler(item["url"])
+
+    def loop(self):
+        # rss_data=feedparser.parse(requests.get("https://mikanime.tv/RSS/Classic").text)
+        rss_data=feedparser.parse(requests.get("http://127.0.0.1:3000").text)
+        rss_list=[]
+        for item in rss_data.entries:
+            rss_list.append({
+                "title": item['id'],
+                "url": item['links'][2]['href'],
+                "length": item['links'][2]['length'],
+            })
+        print("请求rss服务器")
+        if len(self.pre_ls)==0 and len(self.ls)==0:
+            self.ls=rss_list
+        else:
+            self.pre_ls=self.ls
+            self.ls=rss_list
+            self.judge()
+
+    async def mainLoop(self):
+        while True:
+            self.loop()
+            await asyncio.sleep(5)
+
+
 
 if __name__ == "__main__":
-    print(parameters.aira_link)
+    asyncio.run(Server().mainLoop())
--- parameters.py
+++ parameters.py
@@ -4,16 +4,16 @@
 aria_link="http://192.168.31.236:16800/jsonrpc"
 
 # Aria密钥，没有留空
-aria_secret=""
+aria_secret="prc_password"
 
 # 如果是以下字符串为开头的内容进行下载
 start_with=[
-    "[银色子弹字幕组][名侦探柯南]"
+    "[BML] 终末列车寻往何方"
 ]
 
 # 排除出现以下字符串
 exclude=[
-    "繁", "MKV", "v2"
+    "繁", "MKV", "v2", "CHS&CHT"
 ]
 
 # 更新频率 (单位为分钟)
