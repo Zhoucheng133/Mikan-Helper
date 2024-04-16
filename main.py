6c7207123148f3e0c957fd4063d5e9b5afd8896e
zhoucheng <zhoucheng133@outlook.com>
2024-04-16 18:50:07 +0800

    添加API

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import parameters
 import requests
 import feedparser
 import asyncio
+from flask import Flask
+import datetime
+from threading import Thread
 
+log=[]
 
 class Server:
     pre_ls=[]
@@ -27,15 +31,26 @@ class Server:
         '''
         requests.post(parameters.aria_link, data)
 
+    def setLog(self, logtype, val):
+        current_time=datetime.datetime.now()
+        if len(log)>=10:
+            log.pop(0)
+        log.append({
+            "type": logtype,
+            "time": current_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "val": val
+        })
+
     def judge(self):
         newItems = [item for item in self.ls if item not in self.pre_ls]
         download_ls=[]
         for item in newItems:
             if not any(exclude in item['title'] for exclude in parameters.exclude):
-                if any(item['title'].startswith(prefix) for prefix in parameters.start_with):
+                if any(item['title'].replace("  ", " ").startswith(prefix) for prefix in parameters.start_with):
                     download_ls.append(item)
         for item in download_ls:
             print("下载: "+item["title"])
+            self.setLog("ok", "下载: "+item["title"])
             self.downloadHandler(item["url"])
 
     def rssRequest(self):
@@ -43,6 +58,7 @@ class Server:
             rss_data=feedparser.parse(requests.get("https://mikanime.tv/RSS/Classic").text)
             return rss_data
         except requests.RequestException as e:
+            self.setLog("err", "请求出错")
             print("请求出现错误")
             return ""
 
@@ -58,6 +74,7 @@ class Server:
                 "length": item['links'][2]['length'],
             })
         print("请求rss服务器")
+        self.setLog("ok", "请求rss服务器")
         if len(self.pre_ls)==0 and len(self.ls)==0:
             self.ls=rss_list
         else:
@@ -70,7 +87,16 @@ class Server:
             self.loop()
             await asyncio.sleep(parameters.update_freq*60)
 
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return log
 
 if __name__ == "__main__":
-    asyncio.run(Server().mainLoop())
+
+    t1 = Thread(target=asyncio.run, args=(Server().mainLoop(),))
+    t2 = Thread(target=app.run, args=("0.0.0.0", 8811))
+
+    t1.start()
+    t2.start()
