4d8b2ceb6679cba702f70382ddad9ecc4d8c6ced
zhoucheng <zhoucheng133@outlook.com>
2024-04-16 19:14:33 +0800

    完成ui页面

--- connector-ui/src/App.tsx
+++ connector-ui/src/App.tsx
@@ -1,9 +1,44 @@
-
+import { useEffect, useState } from "react"
+import "./style.css"
+import axios from "axios"
 
 function App() {
+  
+  useEffect(()=>{
+    getLog()
+  }, [])
+
+  interface log{
+    time: string,
+    type: string,
+    val: string
+  }
+
+  const [list, setList]=useState<log[]>([]);
+
+  const getLog=async ()=>{
+    const response=await axios.get("http://localhost:8811/api");
+    // console.log(response.data);
+    setList(response.data);
+  }
 
   return (
-    <div>Hello</div>
+    <div className="bg">
+      <div className="logPanel">
+        <div className="title">Mikan Connector Log Panel</div>
+        <div className="logList">
+          {
+            list.map((item)=>{
+              return <div className="logItem" key={item.time}>
+                <div className="logType" style={{color: item.type=="ok"?"lime":"red"}} >{item.type}</div>
+                <div className="logVal">{item.val}</div>
+                <div className="logTime">{item.time}</div>
+              </div>
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
