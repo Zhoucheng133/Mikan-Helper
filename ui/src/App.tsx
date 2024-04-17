b31e98c8dfa76fd153be92a551c9371004a8ed9b
zhoucheng <zhoucheng133@outlook.com>
2024-04-17 19:34:07 +0800

    完成筛选功能

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -15,20 +15,59 @@ function App() {
   }
 
   const [list, setList]=useState<log[]>([]);
+  const [selected, setSelected]=useState<string>("");
+
+  const filterItem=(val: log)=>{
+    if(selected==""){
+      return true;
+    }else if(selected=="ok"){
+      if(val.type=="ok"){
+        return true;
+      }else{
+        return false;
+      }
+    }else if(selected=="err"){
+      if(val.type=="err"){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      if(val.val.includes("下载")){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  }
 
   const getLog=async ()=>{
-    const response=await axios.get("/api");
+    // const response=await axios.get("/api");
+    const response=await axios.get("http://localhost:3000/item");
     // console.log(response.data);
     setList(response.data.reverse());
+  }
+
+  const filter=(val: string)=>{
+    if(selected==val){
+      setSelected("")
+    }else{
+      setSelected(val);
+    }
   }
 
   return (
     <div className="bg">
       <div className="logPanel">
-        <div className="title">Mikan Connector Log Panel</div>
+        <div className="titleBar">
+          <div className="title">Mikan Connector Log Panel</div>
+          <div className={selected=="err" ? "bt_selected" : "bt"} onClick={()=>filter("err")} style={{marginLeft: "auto"}}>失败内容</div>
+          <div className={selected=="ok" ? "bt_selected" : "bt"} onClick={()=>filter("ok")}>成功内容</div>
+          <div className={selected=="download" ? "bt_selected" : "bt"} onClick={()=>filter("download")}>下载内容</div>
+        </div>
         <div className="logList">
           {
-            list.map((item)=>{
+            list.filter((item)=>filterItem(item)).map((item)=>{
               return <div className="logItem" key={item.time}>
                 <div className="logType" style={{color: item.type=="ok"?"lime":"red"}} >{item.type}</div>
                 <div className="logVal">{item.val}</div>
