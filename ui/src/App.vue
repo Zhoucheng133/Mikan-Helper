7968f3bdf85193f343c0979c3ae1384cfb584333
zhoucheng <zhoucheng133@outlook.com>
2024-04-19 21:16:40 +0800

    添加Aria设置页面

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,8 +4,8 @@
       <a-menu v-model:selectedKeys="stores().nowPage" mode="horizontal" :items="nav().items" style="user-select: none;" />
       <div class="content">
         <div v-show="stores().nowPage[0]=='settings'" ><Settings/></div>
+        <div v-show="stores().nowPage[0]=='aria'"><Aria /></div>
         <div v-show="stores().nowPage[0]=='logs'"><Logs /></div>
-        
       </div>
     </ConfigProvider>
   </div>
@@ -15,6 +15,7 @@
 import nav from './stores/nav';
 import stores from './stores';
 import Settings from './components/Settings.vue';
+import Aria from './components/Aria.vue';
 import Logs from './components/Logs.vue';
 import { ConfigProvider } from 'ant-design-vue';
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
