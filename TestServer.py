e9b65df8c9773ae43442f2b1c6933a73c0793708
周诚 <zhoucheng133@outlook.com>
2024-06-30 18:34:46 +0800

    添加测试内容

---
+++ TestServer.py
@@ -0,0 +1,13 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    xml_content=''
+    with open('ui/src/Test/after.xml', 'r', encoding='utf-8') as file:
+        xml_content = file.read()
+    return Response(xml_content, mimetype='application/xml')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
