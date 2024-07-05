5344039358d9df3bad87975490b5ab7f1204b719
周诚 <zhoucheng133@outlook.com>
2024-07-05 21:05:53 +0800

    更换Antd语言

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <ConfigProvider locale={zhCN}>
+        <App />
+    </ConfigProvider>
+    
 )
