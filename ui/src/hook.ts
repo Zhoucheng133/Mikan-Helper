b7d2ff5b2f8f371f60fe13ad6d683ee09b01083d
周诚 <zhoucheng133@outlook.com>
2024-07-06 19:46:02 +0800

    更新url地址

--- ui/src/hook.ts
+++ ui/src/hook.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 
-const baseURL="http://127.0.0.1:8811"
-// const baseURL="http://192.168.31.236:8811";
+const baseURL=""
 
 async function initState(){
   const formData=await axios.get(`${baseURL}/api/status`);
