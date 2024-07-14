3235760226e4d13e292df9f6f3fcb11376e0cd50
周诚 <zhoucheng133@outlook.com>
2024-07-14 18:46:42 +0800

    格式化代码

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,8 +4,8 @@ import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ConfigProvider locale={zhCN}>
-        <App />
-    </ConfigProvider>
-    
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>
+
 )
